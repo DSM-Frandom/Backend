dbb23f8e0d8fae1a59badee2acecbdcbe1130a51
sonchaegeon <thscorjs1002@naver.com>
2021-01-18 19:01:30 +0900

    UPDATE; add morgan, express.json, cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express, { Express, Request, Response, NextFunction } from "express";
-const app: Express = express();
+import express , { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-app.get('/', (req: Request, res: Response): Response => {
-    return res.send("Hello;");
+const app: express.Application = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send("Hello;");
 })
 
 app.listen(3000, () => {
