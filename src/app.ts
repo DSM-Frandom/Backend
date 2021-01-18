91290f10fa26aca79c629034003b58f7b806c738
sonchaegeon <thscorjs1002@naver.com>
2021-01-19 03:51:49 +0900

    UPDATE; error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express , { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import createError, { HttpError } from "http-errors";
 
 const app: express.Application = express();
 
@@ -8,8 +9,23 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
+
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send("Hello;");
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(new createError.NotFound());
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
 })
 
 app.listen(3000, () => {
