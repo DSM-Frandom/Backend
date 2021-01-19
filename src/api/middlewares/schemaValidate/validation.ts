3cc60f65d3d3b18801221fd9badc31111f24badf
sonchaegeon <thscorjs1002@naver.com>
2021-01-20 02:36:33 +0900

    CREATE; validation middleware

---
+++ src/api/middlewares/schemaValidate/validation.ts
@@ -0,0 +1,24 @@
+import { Schema } from "joi";
+import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
+
+const verify = async ({ schema, value }: { schema: Schema; value: any}) => {
+    await schema.validateAsync(value);
+}
+
+export enum Property {
+    BODY = "body",
+    PARAMS = "params",
+    HEADERS = "headers",
+    QUERY = "query",
+}
+
+export default ({ schema, property }: { schema: Schema; property: Property}) => 
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await verify({ schema, value: req[property] });
+            next();
+        } catch {
+            next(new createHttpError.BadRequest("Invalid parameter"));
+        }
+}
