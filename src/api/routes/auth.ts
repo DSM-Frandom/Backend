b1eec13fc81d565b3a526133a46fa8fca3cade2f
sonchaegeon <thscorjs1002@naver.com>
2021-01-20 02:35:40 +0900

    UPDATE; joi validation

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from "express";
 import AuthController from "../../controllers/auth";
+import { userSchema } from "../middlewares/schemaValidate";
+import validation, { Property } from "../middlewares/schemaValidate/validation";
 import tryCatchHandler from "../middlewares/tryCatchHandler"; 
 const route = Router();
 
@@ -8,8 +10,12 @@ export default (app: Router) => {
 
     app.use("/auth", route);
 
-    route.post("/register", tryCatchHandler(authController.register));
-    
+    route.post(
+        "/register",
+        validation({ schema: userSchema, property: Property.BODY}),
+        tryCatchHandler(authController.register)
+    );
+
     route.get("/check", (req: Request, res: Response) => {
         res.status(200).json();
     });
