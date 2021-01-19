598f42159118bfa971d9343e0cc9e4d1679d5929
sonchaegeon <thscorjs1002@naver.com>
2021-01-20 00:34:36 +0900

    UPDATE; register api

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,9 +1,15 @@
 import { Request, Response, Router } from "express";
+import AuthController from "../../controllers/auth";
+import tryCatchHandler from "../middlewares/tryCatchHandler"; 
 const route = Router();
 
 export default (app: Router) => {
+    const authController = new AuthController();
+
     app.use("/auth", route);
 
+    route.post("/register", tryCatchHandler(authController.register));
+    
     route.get("/check", (req: Request, res: Response) => {
         res.status(200).json();
     });
