396e4bb5497ea28449caf1e5df3584c3296a09aa
sonchaegeon <thscorjs1002@naver.com>
2021-01-20 04:27:08 +0900

    UPDATE; password encryption

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import { getRepository } from "typeorm";
 import { User } from "../models/user"
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 
 export default class AuthController {
     public register = async (req: Request, res: Response, next: NextFunction) => {
-        const { email } = req.body;
+        const { email, password, username } = req.body;
 
         const userRepository = getRepository(User);
         const exUser = await userRepository.findOne({
@@ -18,8 +19,14 @@ export default class AuthController {
             next(new createHttpError.Conflict(`${email} is already been registered`));
             return;
         }
-        
-        const newUser = await userRepository.create(req.body);
+
+        const hash = await bcrypt.hash(password, 12);
+
+        const newUser = await userRepository.create({
+            email: email,
+            password: hash,
+            username: username,
+        });
         const savedUser = await userRepository.save(newUser);
         return res.status(201).json(savedUser);
     }
