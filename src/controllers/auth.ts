8f84f80ecaadb350beb9be018b8d330dabbf182f
sonchaegeon <thscorjs1002@naver.com>
2021-01-22 16:29:05 +0900

    UPDATE; generate refresh token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { getRepository } from "typeorm";
 import { User } from "../models/user.entity";
-import { signAccessToken } from "./functions/signToken";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
+import config from "../config";
+import { UserLogin } from "../interfaces";
+import AuthService from "../services/authService";
 
 export default class AuthController {
+    private authService = new AuthService(config.jwtSecret);
+
     public register = async (req: Request, res: Response, next: NextFunction) => {
         const { email, password, username } = req.body;
         const userRepository = getRepository(User);
@@ -26,26 +30,18 @@ export default class AuthController {
             password: hash,
             username: username,
         });
-        const savedUser = await userRepository.save(newUser);
-        const accessToken = await signAccessToken(savedUser.id);
-        return res.status(201).json({ accessToken });
+
+        userRepository.save(newUser).then(() => {
+            return res.status(201).json({
+                message: "Signup successfully"
+            });
+        });
     }
 
     public login = async (req: Request, res: Response, next: NextFunction) => {
-        const { email, password } = req.body;
+        const userLogin: UserLogin = req.body;
         const userRepository = getRepository(User);
-        const promise_exUser: Promise<User | null> = userRepository.findOne({ where: { email: email } });
-
-        const exUser: User | null = await promise_exUser;
-        if(!exUser) {
-            throw new createHttpError.Unauthorized(`${email} is not found`);
-        }
-        const pwdCmp: boolean = bcrypt.compareSync(password, exUser.password);
-        if(!pwdCmp) {
-            throw new createHttpError.Unauthorized("Password dose not match");
-        }
-
-        const accessToken = await signAccessToken(exUser.id);
-        res.status(201).json({ accessToken });
+        const tokens = await this.authService.signIn(userLogin, userRepository);
+        return res.status(200).json(tokens);
     }
 }
