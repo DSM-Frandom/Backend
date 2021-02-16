1587675318b25e7d26c1f1fafd23f2fd39c639b1
sonchaegeon <thscorjs1002@naver.com>
2021-02-17 03:08:20 +0900

    CREATE; report entity

---
+++ src/models/report.entity.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, getConnection, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from ".";
+import { CreateReportDto } from "./report.dto";
+
+@Entity()
+export class Report {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 80 })
+    title: string;
+
+    @Column({ type: "text" })
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @ManyToOne(() => User, user => user.report)
+    @JoinColumn({ name: "user_id" })
+    user: User;
+
+    static async createReport(dto: CreateReportDto, user: User) {
+        const reportRepository = getConnection().getRepository(Report);
+        const newReport = reportRepository.create({
+            ...dto,
+            user
+        });
+        await reportRepository.save(newReport);
+    }
+}
