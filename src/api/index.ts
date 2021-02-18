3786b9157c0c5676c22ef32354689e37b76739b0
sonchaegeon <thscorjs1002@naver.com>
2021-02-18 09:56:19 +0900

    CREATE; file router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { auth, user } from "./routes";
+import { auth, file, user } from "./routes";
 
 export default () => {
     const app = Router();
 
     auth(app);
     user(app);
+    file(app);
 
     return app;
 }
--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import AuthController from "../../controllers/auth";
 import { registerSchema, loginSchema, refreshSchema } from "../middlewares/schemaValidate";
 import validation, { Property } from "../middlewares/validation";
