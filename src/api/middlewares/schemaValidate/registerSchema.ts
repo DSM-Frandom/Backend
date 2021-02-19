d597a69cb34263ba6d3d4621730f2330e5b1cefd
sonchaegeon <thscorjs1002@naver.com>
2021-02-19 12:24:07 +0900

    UPDATE; register schema add property age, gender

--- src/api/middlewares/schemaValidate/registerSchema.ts
+++ src/api/middlewares/schemaValidate/registerSchema.ts
@@ -3,5 +3,7 @@ import Joi from "joi";
 export default Joi.object().keys({
     email: Joi.string().email().lowercase().required(),
     password: Joi.string().min(6).required(),
-    username: Joi.string().min(2).required()
+    username: Joi.string().min(2).required(),
+    age: Joi.number().min(1).max(100).required(),
+    gender: Joi.string().valid('M', 'W').required(),
 }).unknown()
