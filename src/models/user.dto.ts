e1fa427884b8cb5de959b22787d89f69f7dd17d2
sonchaegeon <thscorjs1002@naver.com>
2021-02-19 12:23:51 +0900

    UPDATE; ccreate user dto add property age, gender

--- src/models/user.dto.ts
+++ src/models/user.dto.ts
@@ -1,3 +1,5 @@
+import { Gender } from "./user.entity";
+
 export class UserLoginDto {
     email: string;
     password: string;
@@ -5,4 +7,6 @@ export class UserLoginDto {
 
 export class CreateUserDto extends UserLoginDto {
     username: string;
+    age: number;
+    gender: Gender;
 }
