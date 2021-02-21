cdcbd13d6848225d5608773ba1e2218d32f79c69
sonchaegeon <thscorjs1002@naver.com>
2021-02-22 00:19:53 +0900

    UPDATE; register schema validation

--- src/api/middlewares/schemaValidate/registerSchema.ts
+++ src/api/middlewares/schemaValidate/registerSchema.ts
@@ -11,18 +11,21 @@ export default Joi.object().keys({
         .messages({
             'string.base': error.EMAIL,
             'string.email': '실제 존재하는 이메일이 아닙니다',
+            'string.empty': error.EMAIL,
             'any.required': error.EMAIL,
         }),
     password: Joi.string().min(6).required()
         .messages({
             'string.base': error.PASSWORD,
             'string.min': '비밀번호는 6자 이상으로 해야합니다',
+            'string.empty': error.PASSWORD,
             'any.required': error.PASSWORD,
         }),
     username: Joi.string().min(2).required()
         .messages({
             'string.base': error.USERNAME,
             'string.min': '닉네임은 2자 이상으로 해야합니다',
+            'string.empty': error.USERNAME,
             'any.required': error.USERNAME,
         }),
     age: Joi.number().min(1).max(100).required()
@@ -30,6 +33,7 @@ export default Joi.object().keys({
             'number.base': error.AGE,
             'number.min': '1살 이상으로 해야합니다',
             'number.max': '100살 이하로 해야합니다',
+            'string.empty': error.AGE,
             'any.required': error.AGE
         }),
     gender: Joi.string().valid('M', 'W').required(),
