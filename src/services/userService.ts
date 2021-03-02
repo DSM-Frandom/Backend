d79833bb677eea4efc113b030b5fd4f2124682f4
sonchaegeon <thscorjs1002@naver.com>
2021-03-02 13:00:32 +0900

    UPDATE; user dislike service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -23,8 +23,29 @@ export default class UserService {
     }
 
     public async like(username: string, id: number): Promise<void> {
-        const target = await User.findUserByUsername(username);
         const user = await User.getRepository().findOne(id);
+        const like = await this.findLikeByUser(username, user);
+        const likeHasUserRecord = await LikeHasUser.findLikeHasUserByUserAndLike(user, like);
+        if(likeHasUserRecord) {
+            await LikeHasUser.getRepository().delete(likeHasUserRecord);
+        }
+        await LikeHasUser.createLikeHasUser(user, like, 1);
+        await Like.getRepository().update(like, { count: like.count + 1 });
+    }
+
+    public async dislike(username: string, id: number): Promise<void> {
+        const user = await User.getRepository().findOne(id);
+        const like = await this.findLikeByUser(username, user);
+        const likeHasUserRecord = await LikeHasUser.findLikeHasUserByUserAndLike(user, like);
+        if(likeHasUserRecord) {
+            await LikeHasUser.getRepository().delete(likeHasUserRecord);
+        }
+        await LikeHasUser.createLikeHasUser(user, like, -1);
+        await Like.getRepository().update(like, { count: like.count - 1 });
+    }
+
+    private async findLikeByUser(username: string, user: User): Promise<Like> {
+        const target = await User.findUserByUsername(username);
         if(!target || !user) {
             throw new createHttpError.NotFound('User not found');
         }
@@ -33,11 +54,6 @@ export default class UserService {
         }
 
         const like = await Like.getRepository().findOne({ user: target });
-        const likeHasUserRecord = await LikeHasUser.findLikeHasUserByUserAndLike(user, like);
-        if(likeHasUserRecord) {
-            throw new createHttpError.BadRequest('Already liked user');
-        }
-        await LikeHasUser.createLikeHasUser(user, like, 1);
-        await Like.getRepository().update(like, { count: like.count + 1 });
+        return like;
     }
 }
